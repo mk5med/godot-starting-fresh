e48effd6c3d0a3f308ff26a58f371b5bb09caf7f
Author <Author@Author.com>
2023-09-26 21:42:17 -0400

    Create image to map generator

---
+++ components/map_generator/map_generator.gd
@@ -0,0 +1,37 @@
+@tool
+extends TileMap
+
+var _texture: CompressedTexture2D
+@export var mapTexture: CompressedTexture2D:
+	set(val):
+		_texture = val
+		# Update the map when it is updated
+		createMap()
+		pass
+	get:
+		return _texture
+
+func _init():
+	createMap()
+
+func _ready():
+	createMap()
+
+func createMap():
+	if _texture == null:
+		return
+	self.clear()
+	var w = mapTexture.get_width()
+	var h = mapTexture.get_height()
+
+	var data = mapTexture.get_image()
+
+	for x in range(w):
+		for y in range(h):
+			if data.get_pixel(x,y) == Color.BLACK:
+				self.set_cell(
+					0,
+					Vector2(x, y),
+					0,
+					Vector2(0,0)
+				)
